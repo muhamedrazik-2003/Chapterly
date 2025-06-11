6d1bf3b7cb0766e2ffee0fe4f006b0b1afd29ce4
Muhamed Razik <muhammedrazik987@gmail.com>
2025-06-11 21:01:47 +0530

    feat: created BookCard component for displaying book details

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import BookCard from '../components/BookCard'
 
 const Home = () => {
     return (
@@ -14,6 +15,18 @@ const Home = () => {
                     type="search"
                     placeholder='What are you reading today? Search Chapterly...' 
                     className='bg-slate-900 px-4 cursor-pointer border border-secondary rounded-full h-9 w-xl focus:outline-none focus:ring-2 focus:ring-indigo-600 ' />
+            </section>
+            <section className='md:w-6xl mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-y-10 justify-center'>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
             </section>
         </main>
     )
